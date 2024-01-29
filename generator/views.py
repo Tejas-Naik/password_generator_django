407d3fe7e9c183f897868809404f073655455906
Tejas Naik <RNTejas2005@gmail.com>
2024-01-29 15:20:24 +0530

    Creating a password form and generating random passwords

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import random 
 
 # Create your views here.
 def home(request):
     return render(request, 'generator/home.html', {"password": "saldjf452"})
 
-def eggs(request):
-    return HttpResponse("<h1>Eggs are so tasty</h1>")
+def password(request):
+
+    characters = 'qwertyuiopasdfghjklzxcvbnm'
+
+    length = 10
+
+    thepassword = ''
+    for i in range(length):
+        thepassword +=  random.choice(characters)
+        
+
+    return render(request, 'generator/password.html', {'password':thepassword})
